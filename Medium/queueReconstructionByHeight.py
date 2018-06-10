2c9770b9f4ac43bbbe058f0d8a9362bd8574cea9
konradgnat <konradmgnat@gmail.com>
2018-06-10 06:41:44 -0500

    add med

--- Medium/queueReconstructionByHeight.py
+++ Medium/queueReconstructionByHeight.py
@@ -1,5 +1,7 @@
 '''
-Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k),
+where h is the height of the person and k is the number of people in front of this person who have a height greater
+ than or equal to h. Write an algorithm to reconstruct the queue.
 
 Note:
 The number of people is less than 1,100.
@@ -14,7 +16,7 @@ Output:
 [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
 '''
 
-
+# solution credit goes to leetcode user 'inctrl'
 
 class Solution:
     def reconstructQueue(self, people):
---
+++ Medium/singleNumberIII.py
@@ -0,0 +1,42 @@
+"""
+Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+Example:
+
+Input:  [1,2,1,3,2,5]
+Output: [3,5]
+Note:
+
+The order of the result is not important. So in the above example, [5, 3] is also correct.
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+
+"""
+
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        out = {}
+        ans = []
+        for m, p in enumerate(nums):
+          if p in out:
+            out[p] += 1
+          else:
+            out[p] = 1
+        for k in out:
+          if out[k] == 1:
+            ans.append(k)
+        return ans
+
+
+
+
+# Input:
+dataIn  = [1,2,1,3,2,5]
+# Output: [3,5]
+
+solved = Solution()
+
+print(solved.singleNumber(dataIn))
