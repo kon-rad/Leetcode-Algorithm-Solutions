27ec1c9cb989c0642d97c5b862ed151ed791d75a
konradgnat <konradmgnat@gmail.com>
2019-04-02 21:01:52 -0500

    easy

--- Easy/242-valid-anagram.js
+++ Easy/242-valid-anagram.js
@@ -14,40 +14,28 @@
 // Follow up:
 // What if the inputs contain unicode characters? How would you adapt your solution to such case?
 
-
-
-
 /**
  * Hash map solution, O(m+n) time
- * first create dictionary of all characters in s string
- * then iterate over chars in t string, subtracting occurances from dict
- * if at the end some do not equal zero, it is not anagram
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  const dict = {};
-  s.split('').forEach(c => {
-      if (dict.hasOwnProperty(c)) {
-          dict[c]++;
-      } else {
-          dict[c] = 1;
-      }
-  });
-  t = t.split('');
-  for (let i = 0, l = t.length; i<l; i++) {
-      if (dict.hasOwnProperty(t[i]) && dict[t[i]] >= 0) {
-          dict[t[i]]--;
-      } else {
-          return false;
-      }
+  const sStore = {};
+  const tStore = {};
+  const sLen = s.length;
+  const tLen = t.length;
+  if (sLen !== tLen) {
+      return false;
   }
-  for (c in dict) {
-      if (dict.hasOwnProperty(c)) {
-          if (dict[c] !== 0) {
-              return false;
-          }
+  
+  for (let i = 0; i < sLen; i++) {
+      sStore[s[i]] = sStore[s[i]] + 1 || 1;
+      tStore[t[i]] = tStore[t[i]] + 1 || 1;
+  }
+  for (let key in sStore) {
+      if (sStore[key] !== tStore[key]) {
+          return false;
       }
   }
   return true;
