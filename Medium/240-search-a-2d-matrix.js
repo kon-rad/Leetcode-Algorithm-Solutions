d99f9693657b7cfc29dd2167496d7381c52785ea
konradgnat <konradmgnat@gmail.com>
2019-04-02 20:39:27 -0500

    medium

--- Medium/240-search-a-2d-matrix.js
+++ Medium/240-search-a-2d-matrix.js
@@ -46,3 +46,18 @@ var searchMatrix = function(matrix, target) {
   }
   return false;
 };
+
+function searchMatrix(matrix, target) {
+  if (!matrix.length || !matrix[0].length) return false;
+
+  let row = 0;
+  let col = matrix[0].length - 1;
+
+  while (col >= 0 && row <= matrix.length - 1) {
+    if (matrix[row][col] === target) return true;
+    else if (matrix[row][col] > target) col--;
+    else row++;
+  }
+
+  return false;
+}
