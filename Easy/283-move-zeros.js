a36a0cca569d6fbdc51c9d5a459149d657d36606
konradgnat <konradmgnat@gmail.com>
2019-04-03 20:34:55 -0500

    ea

--- Easy/283-move-zeros.js
+++ Easy/283-move-zeros.js
@@ -12,6 +12,24 @@ You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
  */
 
+ /**
+  * O(n) time - one pass, keep one counter for non zero number position
+  * the second counter is normal incrementor
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  for (let i = 0, l = nums.length, pos = 0; i<l; i++) {
+      if (nums[i] !== 0) {
+          if (i !== pos) {
+              nums[pos] = nums[i];
+              nums[i] = 0;
+          }
+          pos++;
+      }
+  }
+  return nums;
+};
 
 /**
  * @param {number[]} nums
