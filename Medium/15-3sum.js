f5432c13207675b367e0171424e742560de5c86b
konradgnat <konradmgnat@gmail.com>
2019-06-11 06:14:07 -0500

    med

--- Medium/15-3sum.js
+++ Medium/15-3sum.js
@@ -26,36 +26,30 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  let len = nums.length, sum;
+  let len = nums.length, sum, cur, l, r;
   let res = [];
   nums = nums.sort((a, b) => a - b);
 
   for (let i = 0; i<len-2; i++) {
-    while (nums[i] === nums[i+1]) i++;
+    while (nums[i] === nums[i-1]) i++;
     if (nums[i] > 0) break;
-    let cur = nums[i];
-    if (cur === nums[i+1]) continue;
-    let l = i+1;
-    let r = len-1;
+    cur = nums[i];
+    l = i+1;
+    r = len-1;
     while (l < r) {
       sum = nums[l] + nums[r] + cur;
       if (sum === 0) {
         res.push([cur, nums[l], nums[r]]);
-        l++;
-        r--;
         while (nums[l] === nums[l+1] && l < r) l++;
         while (nums[r] === nums[r+1] && l < r) r--;
+        l++;
+        r--;
       } else if (sum < 0) {
         l++;
-        while (nums[l] === nums[l+1] && l < r)  l++;
       } else {
         r--;
-        while (nums[r] === nums[r+1] && l < r) r--;
       }
     }
   }
   return res;
 };
-let arr = [-13,11,11,0,-5,-14,12,-11,-11,-14,-3,0,-3,12,-1,-9,-5,-13,9,-7,-2,9,-1,4,-6,-13,-7,10,10,9,7,13,5,4,-2,7,5,-13,11,10,-12,-14,-5,-8,13,2,-2,-14,4,-8,-6,-13,9,8,6,10,2,6,5,-10,0,-11,-12,12,8,-7,-4,-9,-13,-7,8,12,-14,10,-10,14,-3,3,-15,-14,3,-14,10,-11,1,1,14,-11,14,4,-6,-1,0,-11,-12,-14,-11,0,14,-9,0,7,-12,1,-6];
-let arr2 = [-1, 0, 1, 2, -1, -4];
-console.log(threeSum(arr));
