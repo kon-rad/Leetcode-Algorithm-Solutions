f313344a1f8b5a5bb9ac76428b8e5da0191ffe04
konradgnat <konradmgnat@gmail.com>
2019-06-14 06:37:26 -0500

    med

--- Medium/18-4sum.js
+++ Medium/18-4sum.js
@@ -20,52 +20,39 @@ A solution set is:
 ]
  */
 
-
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
-  const res = [];
   const len = nums.length;
+  const res = [];
+  if (nums === null || len < 4) return res;
   let sum, cur;
-  const map = {};
-  for (let i = 0; i< len; i++) {
-      for (let j = 0; j < len; j++) {
-          sum = nums[i] + nums[j];
-          if (nums[i] > nums[j]) {
-              cur = [nums[j], nums[i]];
-          } else {
-              cur = [nums[i], nums[j]];
-          }
-          if (map.hasOwnProperty(sum)) {
-              map[sum].push(cur);
-          } else {
-              map[sum] = [cur];
+  nums = nums.sort((a, b) => a - b);
+  for (let i = 0; i < len-3; i++) {
+      if (i > 0 && nums[i] === nums[i-1]) continue;
+      for (let j = i+1; j < len - 2; j++) {
+          if (j > i+1 && nums[j] === nums[j-1]) continue;
+          
+          let k = j + 1;
+          let m = len - 1;
+          while (k < m) {
+              sum = nums[i] + nums[j] + nums[k] + nums[m];
+              if (target === sum) {
+                  res.push([nums[i], nums[j], nums[k], nums[m]]);
+                  while (nums[k+1] === nums[k] && k < m) k++;
+                  while (nums[m-1] === nums[m] && k < m) m--;
+                  k++;
+                  m--;
+              } else if (sum < target) {
+                  k++;
+              } else {
+                  m--;
+              }
           }
       }
   }
-  console.log(map);
-  Object.keys(map).forEach(key => {
-      let pair = target - key;
-      if (map.hasOwnProperty(pair)) {
-          console.log('match');
-          map[key].forEach(p1 => {
-              map[pair].forEach(p2 => {
-                  res.push([...p1, ...p2]);
-              })
-          })
-      }
-  })
   return res;
 };
-let nums = [1, 0, -1, 0, -2, 2], target = 0;
-
-fourSum(nums, target);
-// A solution set is:
-// [
-//   [-1,  0, 0, 1],
-//   [-2, -1, 1, 2],
-//   [-2,  0, 0, 2]
-// ]
