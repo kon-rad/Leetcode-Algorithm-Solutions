192af41d1b8da92b182b2b05b3000767ab365696
konrad.gnat <konrad.gnat@backstopsolutions.com>
2019-06-25 21:06:53 -0500

    med

--- Medium/24-swap-nodes-in-pairs.js
+++ Medium/24-swap-nodes-in-pairs.js
@@ -21,7 +21,10 @@ Given 1->2->3->4, you should return the list as 2->1->4->3.
  *     this.next = null;
  * }
  */
+
 /**
+ * Recursive O(N) solution
+ * O(1) space
  * @param {ListNode} head
  * @return {ListNode}
  */
@@ -33,4 +36,26 @@ var swapPairs = function(head) {
     head.next = swapPairs(head.next.next);
     n.next = head;
     return n;
+};
+
+/**
+ * Iterative O(N) solution
+ * O(1) space
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    const dummy = new ListNode(0);
+    let node = dummy;
+    dummy.next = head;
+    while (head && head.next) {
+        let n = head.next;
+        head.next = n.next;
+        n.next = head;
+        
+        node.next = n;
+        node = head;
+        head = node.next;
+    }
+    return dummy.next;
 };
