ebf79c011209208a9823576d5c2f5158639027c0
konrad.gnat <konrad.gnat@backstopsolutions.com>
2019-07-20 11:18:29 -0500

    hard

--- Hard/32-longest-valid-parentheses.js
+++ Hard/32-longest-valid-parentheses.js
@@ -15,31 +15,52 @@ Input: ")()())"
 Output: 4
 Explanation: The longest valid parentheses substring is "()()"
  */
-
- /**
+/**
  * @param {string} s
  * @return {number}
  */
 var longestValidParentheses = function(s) {
-    let open = 0, close = 0, len = s.length;
-    let longest = 0;
+    const len = s.length;
+    let open, close, substringLen, longest = 0;
+    if (len <= 1) {
+        return 0;
+    }
     for (let i = 0; i < len - 1; i++) {
+        // exit early if a longer substring than current is not possible
+        if (longest > len - i) {
+            break;
+        }
+        // reset counts for open and close parenteses
         open = 0;
         close = 0;
         if (s[i] === '(') {
             open++;
         } else {
-            close++;
+            // if the first character is a close parentheses, it is invalid
+            continue;
         }
         for (let j = i + 1; j < len; j++) {
             // check if current section is valid
-            
-            if (Math.abs(open - close) > len - j) {
+            if (s[j] === '(') {
+                open++;
+            } else {
+                close++;
+            }
+            // a parentheses is invalid when:
+            // 1. here are more close brackets than open
+            // 2. more open brackets than there are chars left in the string
+            if (open - close < 0 || Math.abs(open - close) > len - j) {
                 break;
             }
             if (open === close) {
-                
+                // get the length of the substring, add one for index offset
+                substringLen = j - i + 1;
+                // update longest if it has greater length
+                if (substringLen > longest) {
+                    longest = substringLen;
+                }
             }
         }
     }
+    return longest;
 };
