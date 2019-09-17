8404e3d2448db4ece9cbe45e4763660d3517b39c
konrad.gnat <konradmgnat@gmail.com>
2019-09-17 08:00:14 -0500

    sudoku

--- Hard/37-sudoku-solver.js
+++ Hard/37-sudoku-solver.js
@@ -30,18 +30,20 @@ The given board size is always 9x9.
  */
 var solveSudoku = function(board) {
     const sect = [[{}, {}, {}], [{}, {}, {}], [{}, {}, {}]];
-    const rowMem = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
-    const colMem = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+    const rowDict = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+    const colDict = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
     const originalNumbers = {};
     let backtracking = false;
     let breakInnerLoop = false;
+    buildDictionary(board, sect, rowDict, colDict);
 
     for (let i = 0; i < 9; i++) {
       let row = board[i];
-      console.log('board', board);
+      console.log('new row, i', i);
       // if (i === 1) return;
       for (let j = 0; j < 9; j++) {
-        console.log('i, j, row, breakInnerLoop', i, j, row, breakInnerLoop);
+        console.log('i, j, row, backtracking, breakInnerLoop', i, j, row, backtracking, breakInnerLoop);
+        if (i === 2) return;
         if (breakInnerLoop) {
           breakInnerLoop = false;
           console.log('breakInnerLoop', breakInnerLoop);
@@ -59,8 +61,8 @@ var solveSudoku = function(board) {
               originalNumbers[encodeOriginal] = true;
             }
             sect[sectX][sectY][num] = true;
-            rowMem[i][num] = true;
-            colMem[j][num] = true;
+            rowDict[i][num] = true;
+            colDict[j][num] = true;
           }
           continue;
         }
@@ -72,57 +74,52 @@ var solveSudoku = function(board) {
 
         // if backtracking then set count to current num + 1;
         if (backtracking) {
-          count = parseInt(num) + 1;
           backtracking = false;
+          count = parseInt(num) + 1;
+          if (count === 10) {
+            backtracking = true;
+            j = j - 2;
+            if (j < 0) {
+              j = 0;
+              breakInnerLoop = true;
+              if (i === 0) {
+                return [];
+              }
+              i = i - 1;
+              // todo: since break here why is breakInnerLoop flag necessary?
+              break;
+            }
+            continue;
+          }
+          countString = count.toString();
           sect[sectX][sectY][num] = false;
-          rowMem[i][num] = false;
-          colMem[j][num] = false;
+          rowDict[i][num] = false;
+          colDict[j][num] = false;
         }
         while (findingNumber && count < 10 && !backtracking) {
           // todo: while loop get's stuck in infinite
           console.log('count, i, j', countString, i, j);
           // todo find out why gets stuck in loop
-          if (sect[sectX][sectY][countString] || rowMem[i][countString] || colMem[j][countString]) {
+          if (sect[sectX][sectY][countString] || rowDict[i][countString] || colDict[j][countString]) {
             // todo: cache what numbers were already tried & failed
             count++;
             countString = count.toString();
           } else {
-            console.log('found new number', count, sect[sectX][sectY], rowMem, colMem);
+            console.log('found new number', count);
             findingNumber = false;
             sect[sectX][sectY][countString] = true;
-            rowMem[i][countString] = true;
-            colMem[j][countString] = true;
+            rowDict[i][countString] = true;
+            colDict[j][countString] = true;
             board[i][j] = countString;
           }
         }
-        // todo find out why getting empty dots and duplicate values
-
-// num .
-// count, i, j 1 8 2
-// count, i, j 2 8 2
-// count, i, j 3 8 2
-// count, i, j 4 8 2
-// count, i, j 5 8 2
-// count, i, j 6 8 2
-// count, i, j 7 8 2
-// count, i, j 8 8 2
-// count, i, j 9 8 2
-// ten here!!!!! i, j 8 0
-// board [ [ '8', '3', '1', '2', '7', '4', '5', '6', '9' ],
-//   [ '6', '2', '4', '1', '9', '5', '3', '7', '8' ],
-//   [ '5', '9', '8', '3', '6', '.', '.', '6', '.' ],
-//   [ '1', '4', '2', '5', '6', '3', '7', '8', '3' ],
-//   [ '4', '5', '3', '8', '1', '3', '2', '9', '1' ],
-//   [ '7', '6', '9', '4', '2', '.', '.', '.', '6' ],
-//   [ '.', '6', '5', '7', '3', '1', '2', '8', '4' ],
-//   [ '2', '1', '7', '4', '1', '9', '6', '3', '5' ],
-//   [ '3', '8', '.', '.', '8', '.', '.', '7', '9' ] ]
         if (countString === '10') {
           backtracking = true;
           // when backtracking - can't modify original num
-          j = j - (j === 0 ? 1 : 2);
+          j = j - 2;
           console.log('ten here!!!!! i, j', i, j);
           if (j < 0) {
+            j = 0;
             breakInnerLoop = true;
             // back to start, unable to find solution
             if (i === 0) {
@@ -130,19 +127,35 @@ var solveSudoku = function(board) {
             }
 
             i = i - 1;
+            break;
           }
-          break;
         }
         // if backtracking then decrement j by two 
         // todo: if count is 10, then need to go back a step
         // todo: find out how many steps back are needed, or if need to do brute force one by one, maybe need a dictionary of attempts made?
-        
+        console.log('i, j, row', i, j, row, backtracking);
       }
-      console.log('board', board);
     }
 
     return board;
 };
+
+const buildDictionary = (board, sect, rowDict, colDict) => {
+  for (let i = 0; i < 9; i++) {
+    let row = board[i];
+    for (let j = 0; j < 9; j++) {
+      let num = row[j];
+      let sectX = Math.floor(j / 3);
+      let sectY = Math.floor(i / 3);
+      if (num === '.') {
+        continue;
+      }
+      sect[sectX][sectY][num] = true;
+      rowDict[i][num] = true;
+      colDict[j][num] = true;
+    }
+  }
+}
 
 const b = [
   ["8","3",".",".","7",".",".",".","."],
