55dcdefabd910c61bccb9f80a217b24c12b37fc4
konrad.gnat <konradmgnat@gmail.com>
2019-11-13 07:27:51 -0600

    sudoku

--- Hard/37-sudoku-solver.js
+++ Hard/37-sudoku-solver.js
@@ -57,46 +57,25 @@ var solveSudoku = function(board) {
     return solve(board, 0, 0);
 };
 
-var solve = function(board, row, col){
-    
-    for(let i=row; i<9; i++, col=0){
-        for(let j=col; j<9; j++){
-            if(board[i][j] !== '.') continue;
-            for(let c=1; c<=9; c++){
-                console.log('board', board);
-                if(isValid(board, i, j, c.toString())){
-                    board[i][j] = c.toString();
-                    
-                    if(solve(board, i, j+1)){
-                        return true;
-                    }
-                    board[i][j] = '.';
+const solve = (board, i, j) => {
+    for (let y = i; y < 9; y++) {
+        for (let x = j; j < 9; y++) {
+            if (board[x][y] !== '.') continue;
+            for (let num = 1; num < 9; num++) {
+                board[x][y] = num.toString();
+                if (isValid(board, i, j)) {
+                    return solve(board, x, y);
                 }
+                board[x][y] = '.';
             }
-            return false;
         }
     }
-    return true;
 }
 
-var isValid = function(board, x, y, c){
-    let rowStart = Math.floor(x/3) * 3;
-    let colStart = Math.floor(y/3) * 3;
-    
-    for (let i=0; i<9; i++){
-        if(board[i][y] === c || board[x][i] === c) return false;
-    }
-    
-    for(let i = 0; i < 3; i++){
-        for(let j = 0; j < 3; j++){
-            if(board[rowStart+i][colStart+j] === c) return false;
-        }
-    }
+const isValid = (board, i, j) => {
     
     return true;
 }
-
-
 
 
 
