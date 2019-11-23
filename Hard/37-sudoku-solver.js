450099b6e4a158969857645f616fe1068b0c8452
konrad.gnat <konradmgnat@gmail.com>
2019-11-23 16:01:23 -0600

    sudoku

--- Hard/37-sudoku-solver.js
+++ Hard/37-sudoku-solver.js
@@ -23,12 +23,6 @@ You may assume that the given Sudoku puzzle will have a single unique solution.
 The given board size is always 9x9.
 */
 
-// note: get inspo from this solution https://stackoverflow.com/a/19971983/6383727
-// todos: follow this https://www.codefellows.org/blog/sudoku-solver-from-scratch-in-javascript-tdd-style-a-tutorial/
-// lots of info http://www.sudokuwiki.org/sudoku.htm
-// inspo https://leetcode.com/problems/sudoku-solver/discuss/15752/Straight-Forward-Java-Solution-Using-Backtracking
-
-
 const b = [
     ["8", "3", ".", ".", "7", ".", ".", ".", "."],
     ["6", ".", ".", "1", "9", "5", ".", ".", "."],
@@ -42,135 +36,57 @@ const b = [
 ];
 
 
-
-
 const t1 = [
-    ["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]];
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+];
 
 
 
-
-
-
-
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
 var solveSudoku = function(board) {
     return solve(board, 0, 0);
 };
 
-const solve = (board, i, j) => {
-    for (let y = i; y < 9; y++) {
-        for (let x = j; j < 9; y++) {
-            if (board[y][x] !== '.') continue;
-            for (let num = 1; num < 9; num++) {
-                board[y][x] = num.toString();
-                if (isValid(board, y, x)) {
-                    return solve(board, y, x + 1);
+const solve = (board, row, col) => {
+    for (let i = row; i < 9; i++, col = 0) {
+        for (let j = col; j < 9; j++) {
+            if (board[i][j] !== '.') continue;
+            for (let k = 1; k < 10; k++) {
+                if (isValid(board, i, j, k.toString())) {
+                    board[i][j] = k.toString();
+                    if (solve(board, i, j + 1)) {
+                        return true;
+                    }
+                    board[i][j] = '.';
                 }
-                board[y][x] = '.';
             }
-            console.log(board);
             return false;
         }
     }
     return true;
 }
-
-const isValid = (board, y, x) => {
-    const num = board[y][x];
-    const rowStart = Math.floor(x/3) * 3;
-    const colStart = Math.floor(y/3) * 3;
-
-    // check sections
-    for (let m = colStart; m < colStart + 3; m++) {
-        for (let n = rowStart; n < rowStart + 3; n++) {
-            if (m === y && n === x) continue;
-            if (board[m][n] === num) {
-                return false;
-            }
-        }   
+const isValid = (board, row, col, k) => {
+    const rowStart = Math.floor(row / 3) * 3;
+    const colStart = Math.floor(col / 3) * 3;
+    for (let i = 0; i < 9; i++) {
+        if (board[row][i] === k || board[i][col] === k) return false;
     }
-
-    for (let k = 0; k < 9; k++) {
-        // check vertical
-        if (board[k][x] === num && k !== y) {
-            return false;
-        }
-        // check horizontal
-        if (board[y][k] === num && k !== x) {
-            return false;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[rowStart + i][colStart + j] === k) return false;
         }
     }
-
     return true;
 }
-
-
-var isValid = function(board, x, y, c){
-    let rowStart = Math.floor(x/3);
-    let colStart = Math.floor(y/3) * 3;
-    
-    for(let i=0; i<9; i++){
-        if(board[i][y] === c || board[x][i] === c) return false;
-    }
-    
-    for(let i=0; i<3; i++){
-        for(let j=0; j<3; j++){
-            if(board[rowStart+i][colStart+j] === c) return false;
-        }
-    }
-    
-    return true;
-}
-
-
-
-
-
-// var solveSudoku = function(board) {
-//     return solve(board, 0, 0);
-// };
-
-// var solve = function(board, row, col) {
-//     for(let i=row; i<9; i++, col=0){
-//         for(let j=col; j<9; j++){
-//             if(board[i][j] !== '.') continue;
-//             for(let c=1; c<=9; c++){
-//                 if(isValid(board, i, j, c.toString())){
-//                     board[i][j] = c.toString();
-                    
-//                     if(solve(board, i, j+1)){
-//                         return true;
-//                     }
-//                     board[i][j] = '.';
-//                 }
-//             }
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-// var isValid = function(board, x, y, c){
-//     let rowStart = Math.floor(x/3) * 3;
-//     let colStart = Math.floor(y/3) * 3;
-    
-//     for(let i=0; i<9; i++){
-//         if(board[i][y] === c || board[x][i] === c) return false;
-//     }
-    
-//     for(let i=0; i<3; i++){
-//         for(let j=0; j<3; j++){
-//             if(board[rowStart+i][colStart+j] === c) return false;
-//         }
-//     }
-    
-//     return true;
-// }
-
-
 console.log('solution: ', solveSudoku(t1));
-
-
-
-
-
