0826f44c36195848736baa84ab82cfa09bce97a3
konradgnat <konradmgnat@gmail.com>
2020-02-09 21:46:47 -0600

    med

--- Medium/52-jump-game.js
+++ Medium/52-jump-game.js
@@ -21,17 +21,44 @@ Explanation: You will always arrive at index 3 no matter what. Its maximum
              jump length is 0, which makes it impossible to reach the last index.
  */
 
-/**
- * @param {number[]} nums
- * @return {boolean}
+ /**
+ * @param {number} n
+ * @return {number[][]}
  */
-var canJump = function(nums) {
-  let max = nums[0];
-  for (let i = 0; i < nums.length; i++) {
-      if (max < i) {
-          return false;
-      }
-      max = Math.max(i + nums[i], max);
-  }
-  return true;
+var generateMatrix = function(n) {
+    const matrix = [...Array(n)].map(() => Array(n).fill(null));
+    const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    // this determines how many steps can be taken
+    // on the x and y axis
+    const steps = [n, n - 1];
+
+    let num = 1;
+    let dir = 0;
+    let x = 0;
+    // y is set at -1 as the first vertical increment
+    // happends before setting the number
+    let y = -1;
+
+    // dir % 2 determines the direction, either x or y
+    while (steps[dir % 2] > 0) {
+        console.log('steps', steps);
+        console.log('dir', dir);
+        console.log('dir % 2', dir % 2);
+        for (let i = 0; i < steps[dir % 2]; i++) {
+            x += dirs[dir][0];
+            y += dirs[dir][1];
+            // x and y is incremented by the use of
+            // the directions array
+            console.log('x, y, dir', x, y, dir);
+            // add next number in the matrix
+            matrix[x][y] = num++;
+        }
+        // decrement the steps count
+        steps[dir % 2]--;
+        // direction is 0 to 3, as there are 4 directions
+        // (right, down, left, up)
+        dir = (dir + 1) % 4;
+    }
+
+    return matrix;
 };
