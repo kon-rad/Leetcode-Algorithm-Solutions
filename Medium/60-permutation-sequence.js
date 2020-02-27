8ecc758228efda9e8ebd06a0cf3984a5c48628c8
konradgnat <konradmgnat@gmail.com>
2020-02-26 19:40:41 -0600

    med

--- Medium/60-permutation-sequence.js
+++ Medium/60-permutation-sequence.js
@@ -29,32 +29,42 @@ Input: n = 4, k = 9
 Output: "2314"
  */
 
-var getPermutation = function (n, k) {
-  let res = []
-  let f = [1]
-  let s = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-  for (let i = 1; i < n; i++) {
-    console.log('i', i, 'f', f);
-    f[i] = factorial(i)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+  if (!head || !head.next) return head;
+  let dummy = createNode(0);
+  dummy.next = head;
+  let fast = dummy;
+  let slow = dummy;
+  let len = 0;
+  for (len; fast.next !== null; len++) {
+      fast = fast.next;
   }
-  console.log('f after first loop', f);
-  --k
-  for (let i = n; i >= 1; i--) {
-    let j = Math.floor(k / f[i - 1])
-    k %= f[i - 1]
-    res += s[j]
-
-    s.splice(j, 1)
-    console.log('f', f, 'res', res, 's', s);
+  for (let j = len - k % len; j > 0; j--) {
+      slow = slow.next;
   }
-  return res
+  fast.next = dummy.next; // set the head at the end of the first section
+  dummy.next = slow.next; // set the second section head as the new head
+  slow.next = null;
+  
+  return dummy.next;
 };
-
-function factorial(n, total = 1) {
-  if (n === 1) return total;
-  console.log('factorial, totol:', n * total, 'n', n);
-  return factorial(n - 1, n * total);
+const createNode = val => {
+  return {
+      val,
+      next: null,
+  }
 }
-
 console.log(getPermutation(6, 3));
 
