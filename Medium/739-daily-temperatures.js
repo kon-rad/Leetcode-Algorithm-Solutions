33a4f97a2a53450dbbee044f744ee4192dc518f5
konradgnat <konradmgnat@gmail.com>
2020-06-26 08:30:18 -0500

    med

--- Medium/739-daily-temperatures.js
+++ Medium/739-daily-temperatures.js
@@ -8,11 +8,27 @@ For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73]
 
 Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
  */
-
- /**
+/**
  * @param {number[]} T
  * @return {number[]}
  */
 var dailyTemperatures = function(T) {
-    
+  const len = T.length;
+  let max = { val: 0, i: 0 };
+  const result = [];
+  for (let i = len - 1; i >= 0; i--) {
+      if (T[i] >= max.val) {
+          max.val = T[i];
+          max.i = i;
+          result[i] = 0;
+      } else {
+          for (let j = i + 1; j <= max.i; j++) {
+              if (T[j] > T[i]) {
+                  result[i] = j - i;
+                  break;
+              }
+          }
+      }
+  }
+  return result;
 };
