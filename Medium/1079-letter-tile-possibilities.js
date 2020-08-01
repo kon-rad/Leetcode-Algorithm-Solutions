575a0fe23a546824558524bd0f40726d8d99ec74
konradgnat <konradmgnat@gmail.com>
2020-07-31 23:36:25 -0500

    med

--- Medium/1079-letter-tile-possibilities.js
+++ Medium/1079-letter-tile-possibilities.js
@@ -73,3 +73,32 @@ const recursiveComb = (tiles, seenStr, currentStr) => {
   }
   return seenStr;
 }
+
+/**
+ * This one is clean and very nice. 
+ * Todo: how is the count incremented for multi letter combinations? 
+ * Figure out how this works! I can see how one char increments the count, but how do multiple ones?
+ * @param {string} tiles
+ * @return {number}
+ */
+var numTilePossibilities = function(tiles) {
+  if (tiles.length === 0) {
+      return 0;
+  }
+  
+  let count = 0;
+  const set = new Set();
+  
+  // break down tiles and count possible combinations
+  for (let i = 0; i < tiles.length; i++) {
+      // skip if the tile is visited before for duplicated tiles
+      if (set.has(tiles[i])) {
+        continue;
+      }
+      // count all the sub combinations
+      count += numTilePossibilities(tiles.slice(0, i) + tiles.slice(i + 1)) + 1;
+set.add(tiles[i]);
+  }
+  
+  return count;
+};
