b12130c297d1bbb561e2c2cf17efc86735d6676d
konradgnat <konradmgnat@gmail.com>
2020-08-25 19:29:29 -0500

    med

--- Medium/442-find-all-duplicates-in-array.js
+++ Medium/442-find-all-duplicates-in-array.js
@@ -18,5 +18,38 @@ Output:
  * @return {number[]}
  */
 var findDuplicates = function(nums) {
-    
+  const map = new Map();
+  const twice = [];
+  nums.forEach(num => {
+      if (map.has(num)) {
+          twice.push(num);
+      } else {
+          map.set(num, 1);
+      }
+  });
+  return twice;
+};
+
+/**
+ * Mutate the array
+ * Since numbers in array are from 1 to less than n (it's length)
+ * you can indicate that a number has already been seen by making
+ * the value at the index of that item to be negative. Then if you already
+ * see that it is negative, you know it's duplicate and push it to the 
+ * results array.
+ * Space O(n) Time O(n)
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function(nums) {
+  const twice = [];
+  for(let i = 0, len = nums.length; i<len; i++) {
+    let index = Math.abs(nums[i]) - 1;
+    if (nums[index] < 0) {
+      twice.push(Math.abs(nums[i]));
+    } else {
+      nums[index] = nums[index] * -1
+    }
+  }
+  return twice;
 };
