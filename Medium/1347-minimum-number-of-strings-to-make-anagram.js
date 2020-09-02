bea2263931f054bd36502d956c242057cf2624bc
konradgnat <konradmgnat@gmail.com>
2020-09-01 20:48:31 -0500

    med

---
+++ Medium/1347-minimum-number-of-strings-to-make-anagram.js
@@ -0,0 +1,71 @@
+/**
+ * 1347. Minimum Number of Steps to Make Two Strings Anagram
+Medium
+
+Given two equal-size strings s and t. In one step you can choose any character of t and replace it with another character.
+
+Return the minimum number of steps to make t an anagram of s.
+
+An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+ 
+
+Example 1:
+
+Input: s = "bab", t = "aba"
+Output: 1
+Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s.
+Example 2:
+
+Input: s = "leetcode", t = "practice"
+Output: 5
+Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
+Example 3:
+
+Input: s = "anagram", t = "mangaar"
+Output: 0
+Explanation: "anagram" and "mangaar" are anagrams. 
+Example 4:
+
+Input: s = "xxyyzz", t = "xxyyzz"
+Output: 0
+Example 5:
+
+Input: s = "friend", t = "family"
+Output: 4
+ 
+
+Constraints:
+
+1 <= s.length <= 50000
+s.length == t.length
+s and t contain lower-case English letters only.
+Accepted
+ */
+
+ /**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var minSteps = function(s, t) {
+  const tArr = t.split('');
+  const sArr = s.split('');
+  let result = 0;
+  const sMap = new Map();
+  sArr.forEach(c => {
+      sMap.set(c, (sMap.get(c) || 0) + 1);
+  });
+  const tMap = new Map();
+  tArr.forEach(c => {
+      tMap.set(c, (tMap.get(c) || 0) + 1);
+  });
+  tArr.forEach(c => {
+    if (sMap.has(c) && sMap.get(c) > 0) {
+      sMap.set(c, sMap.get(c) - 1);
+    } else {
+      result += 1;
+    }
+  });
+  return result;
+};
