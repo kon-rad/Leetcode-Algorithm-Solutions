2365f1c16a0c477e32390701b82990610800a48a
konradgnat <konradmgnat@gmail.com>
2020-10-10 16:38:39 -0500

    med

---
+++ Medium/515-find-largest-value-in-each-tree-row.js
@@ -0,0 +1,44 @@
+/**
+ * 515. Find Largest Value in Each Tree Row
+Medium
+
+Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
+
+Example 1:
+
+Input: root = [1,3,2,5,3,null,9]
+Output: [1,3,9]
+Example 2:
+
+Input: root = [1,2,3]
+Output: [1,3]
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+Input: root = [1,null,2]
+Output: [1,2]
+Example 5:
+
+Input: root = []
+Output: []
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var largestValues = function(root) {
+    
+};
+
