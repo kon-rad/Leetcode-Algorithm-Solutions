3a99287e2915d6e7878e9da99adcbe3319df2a46
konradgnat <konradmgnat@gmail.com>
2020-11-07 09:58:19 -0600

    easy

---
+++ Easy/914-x-of-a-kind-in-a-deck-of-cards.js
@@ -0,0 +1,91 @@
+/**
+914. X of a Kind in a Deck of Cards
+Easy
+
+665
+
+165
+
+Add to List
+
+Share
+In a deck of cards, each card has an integer written on it.
+
+Return true if and only if you can choose X >= 2 such that it is possible to split the entire deck into 1 or more groups of cards, where:
+
+Each group has exactly X cards.
+All the cards in each group have the same integer.
+ 
+
+Example 1:
+
+Input: deck = [1,2,3,4,4,3,2,1]
+Output: true
+Explanation: Possible partition [1,1],[2,2],[3,3],[4,4].
+Example 2:
+
+Input: deck = [1,1,1,2,2,2,3,3]
+Output: false´
+Explanation: No possible partition.
+Example 3:
+
+Input: deck = [1]
+Output: false
+Explanation: No possible partition.
+Example 4:
+
+Input: deck = [1,1]
+Output: true
+Explanation: Possible partition [1,1].
+Example 5:
+
+Input: deck = [1,1,2,2,2,2]
+Output: true
+Explanation: Possible partition [1,1],[2,2],[2,2].
+ 
+
+Constraints:
+
+1 <= deck.length <= 10^4
+0 <= deck[i] < 10^4
+
+ * 
+ */
+
+ /**
+ * @param {number[]} deck
+ * @return {boolean}
+ */
+var hasGroupsSizeX = function(deck) {
+  const dict = new Map();
+  deck.forEach(num => {
+      if (dict.has(num)) {
+          dict.set(num, [...dict.get(num), num]);
+      } else {
+          dict.set(num, [num]);
+      }
+  });
+  let x = 0;
+  let result = true;
+  for (let arr of dict.values()) {
+      if (x === 0) {
+          x = arr.length;
+          if (x < 2) {
+              result = false;
+              break;
+          }
+      } else {
+          if (x !== arr.length) {
+            if (arr.length % x !== 0) {
+              if (arr.length % 2 === 0 
+                  && x % 2 === 0) {
+                    continue;
+              }
+              result = false;
+              break;
+            }
+          }
+      }
+  };
+  return result;
+};
