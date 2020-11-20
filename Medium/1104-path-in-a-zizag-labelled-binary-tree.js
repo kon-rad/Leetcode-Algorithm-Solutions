ee0c753ae7f433237641390646691e9dda06d77f
konradgnat <konradmgnat@gmail.com>
2020-11-20 07:34:01 -0600

    med

--- Medium/1104-path-in-a-zizag-labelled-binary-tree.js
+++ Medium/1104-path-in-a-zizag-labelled-binary-tree.js
@@ -33,64 +33,57 @@ Constraints:
  * @return {number[]}
  */
 var pathInZigZagTree = function(label) {
-  console.log(label);
-  const result = [];
   let i = 1;
-  let levels = [];
-  while (i < label) {
+  const result = [];
+  const levels = [];
+  while (i <= label) {
     levels.push(i);
     i *= 2;
   }
   const finalLevels = [];
   let c = 0;
   levels.forEach((l, i) => {
-    let newLevel = new Array(l).fill(1);
-    const val = newLevel.map(e => {
+    const newLevel = new Array(l).fill(1).map(e => {
       e += c;
       c++;
       return e;
     });
     if ((i + 1) % 2 === 0) {
-      finalLevels.push(val.reverse());
+      finalLevels.push(newLevel.reverse());
     } else {
-      finalLevels.push(val);
+      finalLevels.push(newLevel);
     }
   });
-  let cur = label;
-  result.unshift(cur);
+  let curLabel = label;
+  result.unshift(curLabel);
+  // the main insight is that if the even rows were not reversed, one could find
+  // the path to the root by getting the next node with the formula Math.floor(node / 2) 
+  // next to find the node in a reversed row, get the distance from the start, 
+  // then get the node at that position from the end of the array
   for (let i = finalLevels.length - 1; i > 0; i--) {
     let isReversed = ((i + 1) % 2 === 0) ? true : false;
-    let nextCur;
-    console.log('isReversed', isReversed);
+    // nextLabel is the next label to be added to results array
+    let nextLabel;
+
+    // if row is even, labels go from right to left
     if (isReversed) {
-      let first = finalLevels[i][0];
-      let diffFromFirst = first - cur;
-      let last = finalLevels[i][finalLevels[i].length - 1];
-      let pos = last + diffFromFirst;
-      nextCur = Math.floor(pos / 2);
+      let firstLabel = finalLevels[i][0];
+      let diffFromFirst = firstLabel - curLabel;
+      let lastLabel = finalLevels[i][finalLevels[i].length - 1];
+      let pos = lastLabel + diffFromFirst;
+      nextLabel = Math.floor(pos / 2);
     } else {
-      nextCur = Math.floor(cur / 2);
-      console.log('finalLevels[i]', finalLevels[i]);
+      // if row is odd, labels go from left to right
+      nextLabel = Math.floor(curLabel / 2);
       let nextLevel = finalLevels[i - 1];
       let firstItemInNextLevel = nextLevel[nextLevel.length - 1];
-      let diff = nextCur - firstItemInNextLevel;
-      nextCur = finalLevels[i - 1][diff];
-      // console.log('nextCurNextLevel', nextCurNextLevel);
-      // console.log('diff', diff);
-      // console.log('nextCur', nextCur);
-      // console.log('firstItemInNextLevel', firstItemInNextLevel);
-      // console.log('finalLevels i', finalLevels, i);
+      let diff = nextLabel - firstItemInNextLevel;
+      nextLabel = finalLevels[i - 1][diff];
     }
-    result.unshift(nextCur);
-    cur = nextCur;
-    console.log(i, cur, result)
+    result.unshift(nextLabel);
+    curLabel = nextLabel;
 
   }
-  // if row is even, labels go from right to left
-  // if row is odd, labels go from left to right
-  // 15 - 12 = 3
-  // 8 + 3 = 11
-  // Math.floor(11/2) = 5
   return result;
 };
 
