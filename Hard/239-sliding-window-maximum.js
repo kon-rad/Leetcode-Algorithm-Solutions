3703fb4cb0c09485d9b266c17a4605609c94c1f5
konradgnat <konradmgnat@gmail.com>
2020-12-02 17:38:52 -0600

    hard

---
+++ Hard/239-sliding-window-maximum.js
@@ -0,0 +1,82 @@
+/**
+ * 
+ * 239. Sliding Window Maximum
+Hard
+
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the max sliding window.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+Example 3:
+
+Input: nums = [1,-1], k = 1
+Output: [1,-1]
+Example 4:
+
+Input: nums = [9,11], k = 2
+Output: [11]
+Example 5:
+
+Input: nums = [4,-2], k = 2
+Output: [4]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+1 <= k <= nums.length
+
+*/
+
+ /**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+  const result = [];
+  let [max, maxI] = getMax(nums, 0, k - 1);
+  for (let i = 0; i <= nums.length - k; i++) {
+      let endIndex = (i + k) - 1;
+      if (maxI < i) {
+        [max, maxI] = getMax(nums, i, (i + k) - 1);
+      } else if (max < nums[endIndex]) {
+        max = nums[endIndex];
+        maxI = endIndex;
+      }
+      result.push(max);
+  }
+  return result;
+};
+
+const getMax = (nums, start, end) => {
+  let max = nums[start];
+  let maxI = start;
+  for (let i = start + 1; i <= end; i++) {
+      if (nums[i] > max) {
+          max = nums[i];
+          maxI = i;
+      }
+  }
+  return [max, maxI];
+}
