d14d769553f9dc68bb9088c81532e40b69208d04
konradgnat <konradmgnat@gmail.com>
2021-02-16 18:34:30 -0600

    easy

---
+++ Easy/1752-check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,72 @@
+/**
+ * 
+ * 1752. Check if Array Is Sorted and Rotated
+Easy
+
+Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+
+There may be duplicates in the original array.
+
+Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: true
+Explanation: [1,2,3,4,5] is the original sorted array.
+You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
+Example 2:
+
+Input: nums = [2,1,3,4]
+Output: false
+Explanation: There is no sorted array once rotated that can make nums.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: true
+Explanation: [1,2,3] is the original sorted array.
+You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+Example 4:
+
+Input: nums = [1,1,1]
+Output: true
+Explanation: [1,1,1] is the original sorted array.
+You can rotate any number of positions to make nums.
+Example 5:
+
+Input: nums = [2,1]
+Output: true
+Explanation: [1,2] is the original sorted array.
+You can rotate the array by x = 5 positions to begin on the element of value 2: [2,1].
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+ */
+
+ /**
+ * Time: O(n) loop over nums once
+ * Space: O(1) constant, only count variable used
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+  let stepDownCount = 0;
+  for (let i = 1, len = nums.length; i < len; i++) {
+      if (nums[i] < nums[i - 1]) {
+          stepDownCount++;
+      }
+  }
+  // observe that only one 'step down' is allowed AND that the last number is less than or greater
+  // then the first
+  // Or if there are no 'step downs' then it is already in non-decreasing order
+  if (stepDownCount === 0 || (stepDownCount === 1 && nums[nums.length - 1] <= nums[0])) {
+      return true;
+  }
+  return false;
+};
+      
