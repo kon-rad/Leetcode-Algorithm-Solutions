c9e15fb7f195eb2f9407de10e380ef3fc3ec1371
konradgnat <konradmgnat@gmail.com>
2021-05-25 22:16:51 -0400

    med

---
+++ Medium/57-insert-interval.js
@@ -0,0 +1,83 @@
+/**
+ * 57. Insert Interval
+Hard
+
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+ */
+
+/**
+ * O(n) time complexity
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function (intervals, newInterval) {
+  const result = [];
+  let i = 0;
+  const len = intervals.length;
+  while (i < len && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i]);
+    i++;
+  }
+  newInterval[0] = Math.min(
+    newInterval[0],
+    i < len ? intervals[i][0] : Infinity
+  );
+  while (i < len && newInterval[1] >= intervals[i][0]) {
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+    i++;
+  }
+  result.push(newInterval);
+  return result.concat(intervals.slice(i));
+};
+
+/**
+ * Time: O(n)
+ * Space: O(n)
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert2 = function (intervals, newInterval) {
+  const start = [];
+  const end = [];
+  let s = newInterval[0],
+    e = newInterval[1];
+  intervals.forEach((int) => {
+    if (int[1] < newInterval[0]) {
+      start.push(int);
+    } else if (int[0] > newInterval[1]) {
+      end.push(int);
+    } else {
+      s = Math.min(int[0], s);
+      e = Math.max(int[1], e);
+    }
+  });
+  return [...start, [s, e], ...end];
+};
+
+console.log(
+  insert(
+    [
+      [1, 2],
+      [3, 5],
+      [6, 7],
+      [8, 10],
+      [12, 16],
+    ],
+    [4, 8]
+  )
+);
