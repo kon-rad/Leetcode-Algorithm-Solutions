d6a35480ade6b131c391eecd276b3ce5632a4da5
konradgnat <konradmgnat@gmail.com>
2021-06-02 20:58:57 -0500

    hackerrank

--- hackerrank/coding/queens-attack-ii.js
+++ hackerrank/coding/queens-attack-ii.js
@@ -100,116 +100,220 @@ Since there is only one square, and the queen is on it, the queen can move 0 squ
  *  5. 2D_INTEGER_ARRAY obstacles
  */
 
-function queensAttack2(n, k, r_q, c_q, obstacles) {
-  // get max possible queen moves
-  let sumMoves = 0;
-  // get moves on right
-  let cellsRight = c_q - 1;
-  // get moves on left
-  let cellsLeft = n - c_q;
-  // get moves above
-  let cellsUp = n - r_q;
-  // get moves below
-  let cellsDown = r_q - 1;
-  // get moves diagonal - up right
-  sumMoves += Math.min(cellsRight, cellsUp);
-  // get moves diagonal - down right
-  sumMoves += Math.min(cellsRight, cellsDown);
-  // get moves diagonal - up left
-  sumMoves += Math.min(cellsLeft, cellsUp);
-  // get moves diagonal - down left
-  sumMoves += Math.min(cellsLeft, cellsDown);
-  sumMoves += cellsRight + cellsLeft + cellsUp + cellsDown;
+function queensAttack(n, k, r_q, c_q, obstacles) {
+  // obstacles closest to queen
+  const up = { r: -1, c: -1 };
+  const down = { r: -1, c: -1 };
+  const right = { r: -1, c: -1 };
+  const left = { r: -1, c: -1 };
 
-  let maxRowRight = 0;
-  let maxRowLeft = 0;
-  let maxColUp = 0;
-  let maxColDown = 0;
+  // up right
+  const ur = { r: -1, c: -1 };
+  // up left
+  const ul = { r: -1, c: -1 };
+  // down right
+  const dr = { r: -1, c: -1 };
+  // down left
+  const dl = { r: -1, c: -1 };
 
-  let maxUpRight = 0;
-  let maxUpLeft = 0;
-  let maxDownRight = 0;
-  let maxDownLeft = 0;
-
-  obstacles.forEach((ob) => {
-    // ob = [r, c]
-    // obstacle is on the same row as queen
-    if (ob[0] === r_q) {
-      // obstacle is right of queen - on the same row
-      if (ob[1] > c_q) {
-        // get the number of squares that are to the right of obstacle, plus 1 for obstacle itself
-        let squaresRemoved = n - ob[1] + 1;
-        maxRowRight = Math.max(squaresRemoved, maxRowRight);
-      } else {
-        // obstacle is left of queen
-        let squaresRemoved = ob[1];
-        maxRowLeft = Math.max(squaresRemoved, maxRowLeft);
+  obstacles.forEach((obs) => {
+    // obs = [row, col]
+    let obs_row = obs[0];
+    let obs_col = obs[1];
+    // obstacle is on the same column as queen
+    if (obs_col === c_q) {
+      if (obs_row > r_q && (obs_row < up.r || up.r === -1)) {
+        // up
+        up.r = obs_row;
+        up.c = obs_col;
+        return;
+      } else if (obs_row < r_q && (obs_row > up.r || up.r === -1)) {
+        // down
+        down.r = obs_row;
+        down.c = obs_col;
+        return;
       }
-    } else if (ob[1] === c_q) {
-      // obstacle is on the same column as queen
-      if (ob[0] > r_q) {
-        // if it is above queen
-        let squaresRemoved = n - ob[0] + 1;
-        maxColUp = Math.max(squaresRemoved, maxColUp);
-      } else {
-        // if it is below queen
-        let squaresRemoved = ob[0];
-        maxColDown = Math.max(squaresRemoved, maxColDown);
-      }
-    } else {
-      // check if obstacle is on diagonal with queen
-      // check if top right diagonal
-      let rowDiff = Math.abs(r_q - ob[0]);
-      let colDiff = Math.abs(c_q - ob[1]);
-      if (rowDiff === colDiff) {
-        // they are on the same diagonal
-        // obstacle is above queen
-        if (ob[0] > r_q) {
-          // obstacle is right of queen
-          if (ob[1] > c_q) {
-            // top right
-            if (ob[0] > ob[1]) {
-              // obstacle is closer to upper wall
-              squaresRemoved = n - ob[0] + 1;
-            } else {
-              squaresRemoved = n - ob[1] + 1;
-            }
-            maxUpRight = Math.max(squaresRemoved, maxUpRight);
-          } else {
-            // top left
-            if (ob[1] > n - ob[0] + 1) {
-              squaresRemoved = n - ob[0] + 1; // closer to top
-            } else {
-              squaresRemoved = ob[1]; // closer to left
-            }
-            maxUpLeft = squaresRemoved > maxUpLeft ? squaresRemoved : maxUpLeft;
-          }
-        } else {
-          // obstacle is below queen
-          if (ob[1] > c_q) {
-            // down right
-            if (ob[0] > n - ob[1]) {
-              squaresRemoved = n - ob[1] + 1;
-            } else {
-              squaresRemoved = ob[0];
-            }
-            maxDownRight =
-              squaresRemoved > maxDownRight ? squaresRemoved : maxDownRight;
-          } else {
-            // down left
-            if (ob[0] < ob[1]) {
-              squaresRemoved = ob[0];
-            } else {
-              squaresRemoved = ob[1];
-            }
-            maxDownLeft =
-              squaresRemoved > maxDownLeft ? squaresRemoved : maxDownLeft;
-          }
-        }
+    } else if (obs_row === r_q) {
+      // it is on the same row
+      if (obs_col > c_q && (obs_col < right.c || right.c === -1)) {
+        // it is to the right
+        right.r = obs_row;
+        right.c = obs_col;
+        return;
+      } else if (obs_col < c_q && (obs_col > left.c || left.c === -1)) {
+        // it is to the left
+        left.c = obs_col;
+        left.r = obs_row;
+        return;
       }
     }
+
+    // down right
+    if (
+      r_q - obs_row === obs_col - c_q &&
+      obs_col > c_q &&
+      obs_row < r_q &&
+      ((obs_row > dr.r && obs_col < dr.c) || dr.r === -1)
+    ) {
+      dr.r = obs_row;
+      dr.c = obs_col;
+      return;
+    }
+
+    // down left
+    if (
+      r_q - obs_row === c_q - obs_col &&
+      obs_col < c_q &&
+      obs_row < r_q &&
+      ((obs_row > dl.r && obs_col > dl.c) || dl.r === -1)
+    ) {
+      dl.r = obs_row;
+      dl.c = obs_col;
+      return;
+    }
+
+    // up right
+    if (
+      obs_row - r_q === obs_col - c_q &&
+      obs_row > r_q &&
+      obs_col > c_q &&
+      ((obs_row < ur.r && obs_col < ur.c) || ur.r === -1)
+    ) {
+      ur.r = obs_row;
+      ur.c = obs_col;
+      return;
+    }
+    // up left
+    if (
+      c_q - obs_col === obs_row - r_q &&
+      obs_col < c_q &&
+      obs_row > r_q &&
+      ((obs_row < ul.r && obs_col > ul.c) || ul.r === -1)
+    ) {
+      ul.r = obs_row;
+      ul.c = obs_col;
+      return;
+    }
   });
-  sumMoves -= maxUpRight + maxUpLeft + maxDownRight + maxDownLeft;
-  sumMoves -= maxRowRight + maxRowLeft + maxColUp + maxColDown;
-  return sumMoves;
+
+  let su = up.r === -1 ? n - r_q : up.r - r_q - 1;
+  let sd = down.r === -1 ? r_q - 1 : r_q - down.r - 1;
+  let sr = right.c === -1 ? n - c_q : right.c - c_q - 1;
+  let sl = left.c === -1 ? c_q - 1 : c_q - left.c - 1;
+
+  // diagonals
+  // squares up right
+  let sur = ur.c === -1 ? Math.min(n - r_q, n - c_q) : ur.c - c_q - 1;
+  let sul = ul.c === -1 ? Math.min(n - r_q, c_q - 1) : c_q - ul.c - 1;
+  let sdr = dr.c === -1 ? Math.min(r_q - 1, n - c_q) : dr.c - c_q - 1;
+  let sdl = dl.c === -1 ? Math.min(c_q - 1, r_q - 1) : c_q - dl.c - 1;
+  return su + sd + sr + sl + sur + sul + sdr + sdl;
 }
+
+let n = 100,
+  r_q = 48,
+  c_q = 81;
+const obs = [
+  [54, 87],
+  [64, 97],
+  [42, 75],
+  [32, 65],
+  [42, 87],
+  [32, 97],
+  [54, 75],
+  [64, 65],
+  [48, 87],
+  [48, 75],
+  [54, 81],
+  [42, 81],
+  [45, 17],
+  [14, 24],
+  [35, 15],
+  [95, 64],
+  [63, 87],
+  [25, 72],
+  [71, 38],
+  [96, 97],
+  [16, 30],
+  [60, 34],
+  [31, 67],
+  [26, 82],
+  [20, 93],
+  [81, 38],
+  [51, 94],
+  [75, 41],
+  [79, 84],
+  [79, 65],
+  [76, 80],
+  [52, 87],
+  [81, 54],
+  [89, 52],
+  [20, 31],
+  [10, 41],
+  [32, 73],
+  [83, 98],
+  [87, 61],
+  [82, 52],
+  [80, 64],
+  [82, 46],
+  [49, 21],
+  [73, 86],
+  [37, 70],
+  [43, 12],
+  [94, 28],
+  [10, 93],
+  [52, 25],
+  [50, 61],
+  [52, 68],
+  [52, 23],
+  [60, 91],
+  [79, 17],
+  [93, 82],
+  [12, 18],
+  [75, 64],
+  [69, 69],
+  [94, 74],
+  [61, 61],
+  [46, 57],
+  [67, 45],
+  [96, 64],
+  [83, 89],
+  [58, 87],
+  [76, 53],
+  [79, 21],
+  [94, 70],
+  [16, 10],
+  [50, 82],
+  [92, 20],
+  [40, 51],
+  [49, 28],
+  [51, 82],
+  [35, 16],
+  [15, 86],
+  [78, 89],
+  [41, 98],
+  [70, 46],
+  [79, 79],
+  [24, 40],
+  [91, 13],
+  [59, 73],
+  [35, 32],
+  [40, 31],
+  [14, 31],
+  [71, 35],
+  [96, 18],
+  [27, 39],
+  [28, 38],
+  [41, 36],
+  [31, 63],
+  [52, 48],
+  [81, 25],
+  [49, 90],
+  [32, 65],
+  [25, 45],
+  [63, 94],
+  [89, 50],
+  [43, 41],
+];
+
+console.log(queensAttack(n, 100, r_q, c_q, obs));
