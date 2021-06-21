25cd557877c18b229f892fd2176b91daa8592714
konradgnat <konradmgnat@gmail.com>
2021-06-21 00:02:07 -0500

    med

--- Medium/63-unique-paths-ii.js
+++ Medium/63-unique-paths-ii.js
@@ -30,10 +30,43 @@ There are two ways to reach the bottom-right corner:
 2. Down -> Down -> Right -> Right
  */
 
- /**
+/**
  * @param {number[][]} obstacleGrid
  * @return {number}
  */
-var uniquePathsWithObstacles = function(obstacleGrid) {
-    
+var uniquePathsWithObstacles = function (obstacleGrid) {
+  if (obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1] === 1) {
+    return 0;
+  }
+  let dp = new Array(obstacleGrid.length)
+    .fill()
+    .map((row) => new Array(obstacleGrid[0].length).fill(0));
+  dp[0][0] = obstacleGrid[0][0] === 0 ? 1 : 0;
+  console.log(dp);
+  for (let i = 0; i < obstacleGrid.length; i++) {
+    for (let j = 0; j < obstacleGrid[0].length; j++) {
+      let up;
+      let left;
+      if (i - 1 >= 0 && obstacleGrid[i - 1][j] !== 1) {
+        up = dp[i - 1][j];
+      } else {
+        up = 0;
+      }
+      if (j - 1 >= 0 && obstacleGrid[i][j - 1] !== 1) {
+        left = dp[i][j - 1];
+      } else {
+        left = 0;
+      }
+      dp[i][j] += +up + left;
+    }
+  }
+  console.log(dp);
+  return dp[dp.length - 1][dp[0].length - 1];
 };
+
+const grid = [
+  [0, 0, 0],
+  [0, 1, 0],
+  [0, 0, 0],
+];
+console.log(uniquePathsWithObstacles(grid));
